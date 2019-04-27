12363308668511b34eea0affb21f72a4ead3b2fd
Eduardo Guerra <46012048+eduardogdc@users.noreply.github.com>
2019-04-27 16:38:48 -0400

    Update and rename social_media_keywords to social_media_keywords.py

--- social_media_keywords.py
+++ social_media_keywords.py
@@ -1,14 +1,22 @@
+# import libraries
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# assign the scope
+# assign a scope
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
+# create credentials
 credaccess = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+
+# authorize a gspread client
 client = gspread.authorize(credaccess)
 
+# access Google Sheets by calling client.open
 sheet = client.open('My Spreadsheet').sheet1
 
+# set data to all records in sheet 1
 data = sheet.get_all_records()
+
+# print to terminal
 print(data)
